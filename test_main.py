e075c3d54a49d69b8e8fd168fb50695519fcbff1
SaiKishore29 <saikishore.saravanan@cdw.com>
2026-04-20 22:59:19 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,4 +6,11 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from FastAPI!"}
+    assert response.json() == "Order Service is running as expected."
+
+def test_create_order():
+    payload = {"customer": "Sai Kishore", "items": ["Laptop", "Monitor"]}
+    response = client.post("/orders", json=payload)
+    assert response.status_code == 201
+    assert "id" in response.json()
+    assert response.json()["customer"] == "Sai Kishore"
